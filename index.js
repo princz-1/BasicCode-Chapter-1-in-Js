e9c2119add5ed2708edbf3e4a589d83d4413914c
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-18 06:40:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Prince");
+console.log("I am from Partapr");
+console.log("I am studing in BCA first year");
+let a = 45;
+
+a = 50;
+let b = 40;
+console.log(a+b);
+
+let Name = "Prince";
+let surname = "Panchal";
+console.log(Name+surname);
+
+const num = 54;
+console.log(num);
+
